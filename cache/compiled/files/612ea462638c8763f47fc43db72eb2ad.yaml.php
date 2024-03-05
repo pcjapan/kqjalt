b59e89c485aafdbe08b07be1c909e06d150e1564
pcjapan <54536915+pcjapan@users.noreply.github.com>
2024-03-05 11:49:59 +0900

    Schedule page modified

--- cache/compiled/files/612ea462638c8763f47fc43db72eb2ad.yaml.php
+++ cache/compiled/files/612ea462638c8763f47fc43db72eb2ad.yaml.php
@@ -2,7 +2,7 @@
 return [
     '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
     'filename' => 'themes://bootstrap4/bootstrap4.yaml',
-    'modified' => 1705362558,
+    'modified' => 1702603851,
     'size' => 80,
     'data' => [
         'navbar_style' => 'navbar-dark',
